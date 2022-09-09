00235c4279ee2ac93e23b81c323b5bc4eb50e8b0
Amrinder <asin211@mywhitecliffe.com>
2022-09-09 21:58:26 +1200

    Bowling Game class checked, few bugs fixed in different functions, comments added for different functions

--- main.py
+++ main.py
@@ -1,8 +1,56 @@
-# File 1 (Test.py)
-# This file has information about test cases which you need to test.
-
 import unittest
-import BowlingGame
+# import BowlingGame
+
+# Bowling Game class (for rolling pins numbers and calculating score)
+class BowlingGame:
+    def __init__(self):
+        self.rolls = []    #empty array to save the pins or numbers
+
+    # Function or method for appending an array where pins will be saved as score, it will take an argument for specific pin or number
+    def roll(self, pins):
+        self.rolls.append(pins)
+
+    # Function for calculation score
+    def score(self):
+        result = 0
+        rollIndex = 0
+        for frameIndex in range(10):
+            # checking for strike with strike function with takes rollIndex as a argument which targets index of an array for comaprison with 10
+            if self.isStrike(rollIndex):
+                result += self.strikeScore(rollIndex)
+                rollIndex += 1
+            elif self.isSpare(rollIndex):
+                result += self.spareScore(rollIndex)
+                rollIndex += 2
+            else:
+                result += self.frameScore(rollIndex)
+                rollIndex += 2
+            return result
+
+    # Function for checking Strike
+    def isStrike(self, rollIndex):
+        return self.rolls[rollIndex] == 10
+
+    # function for checking spare
+    def isSpare(self, rollIndex):
+        return self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10
+
+    # function for score for strike where next 2 numbers in array is added as a bonus with 10 targetting next index of two numbers
+    def strikeScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
+
+    # function for spare score
+    def spareScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 2]
+
+    # function for frame score
+    def frameScore(self, rollIndex):
+        return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
+
+
+
+# File 2 (Test.py)
+# This file has information about test cases which you need to test.
 
 
 class TestBowlingGame(unittest.TestCase):
@@ -46,51 +94,6 @@ class TestBowlingGame(unittest.TestCase):
             self.game.rolls(pins)
 
 
-# File 2 (BowlingGame.py)
-# This file has information about Bowling Game for which the description is provided in project assessment.
-
-class BowlingGame:
-    def __init__(self):
-        self.rolls = []
-
-    def roll(self, pins):
-        self.rolls.append(pins)
-
-    def score(self):
-        result = 0
-        rollIndex = 0
-        for frameIndex in range(10):
-            if frameIndex in range(10):
-                result += self.StrikeScore(rollIndex)
-                rollIndex += 1
-            elif self.isSpare(rollIndex):
-                result += self.spareScore(rollIndex)
-                rollIndex += 2
-            else:
-                result += self.frameScore(rollIndex)
-            rollIndex += 2
-            return result
-
-    def isStrike(self, rollIndex):
-        return self.rolls[rollIndex] == 10
-
-    def isSpare(self, rollIndex):
-        return self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10
-
-    def stickeScore(self, rollIndex):
-        return 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
-
-    def spareScore(self, rollIndex):
-        return 10 + self.rolls[rollIndex + 2]
-
-    def frameScore(self, rollIndex):
-        return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
-
-# Your tasks for code parts:
-# 1: If there are any bugs in the code, you have to remove using debugging and run the project and test cases.
-# 2: Refactor the code (Improve its structure without changing external behaviour).
-# 3: Report everything using github commits and versioning control.
 
 
-###### Important #####
-# Please complete your project and all tasks according to assessment description provided in CANVAS.
+
