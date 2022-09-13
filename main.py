50d6661d3765c1293da73bc9c58610646a9f966d
Amrinder <asin211@mywhitecliffe.com>
2022-09-13 19:33:53 +1200

    Adding more comments

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+Author: Amrinder
+Date Created: 13 Sep 2022
+"""
+
 import unittest    #built-in testing library to test Python code.
 
 # Bowling Game class with functions
@@ -11,20 +16,23 @@ class BowlingGame:
 
     # Function for calculation score
     def score(self):
-        result = 0
-        rollIndex = 0
-        for frameIndex in range(10):
+        result = 0       # variable for score
+        rollIndex = 0    # targeting the index of an array with this variable
+        for frameIndex in range(10):                #for loop is going to run 10 times
             # conditional statements for checking different cases targeting index of an array
             if self.rolls[rollIndex] == 10:
-                result += 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
+                result += 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]     # score target 10 points plus next 2 index numbers (total of these 2 niumbers as a bonus points) as a bonus points
                 rollIndex += 1
             elif self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10:
-                result += 10 + self.rolls[rollIndex + 2]
+                result += 10 + self.rolls[rollIndex + 2]                       #score targeting 10 points plus 1 bonus ball points if 2 ball in a frame are 10
                 rollIndex += 2
             else:
-                result += self.rolls[rollIndex] + self.rolls[rollIndex + 1]
+                result += self.rolls[rollIndex] + self.rolls[rollIndex + 1]     # score targeting just normal ball points
                 rollIndex += 2
-        return result
+        return result        #returning the total score for bowling game
+
+
+
 
 
 # Test Bowling Game class - with different functions for testing different test cases
@@ -36,15 +44,15 @@ class TestBowlingGame(unittest.TestCase):
     # Function with a loop takes 2 arguments: pins = specific number and rolls = number of times of loop
     def rollMany(self, pins, rolls):
         for i in range(rolls):
-            self.game.roll(pins)                  # Calling roll function from Bowling class
+            self.game.roll(pins)                  # Calling roll function from Bowling class for 1 bowl
 
     # Function for testing all zeros
     def testGutterGame(self):
-        self.rollMany(0, 20)
+        self.rollMany(0, 20)                      #calling rollMany function from which will run 20 times and 0 pins each time
         assert self.game.score() == 0             #assert: conditional test returns True, if not, the program will raise an AssertionError
-        print(self.game.score())                  # calling score function from Bowling Class with game object
+        print(self.game.score())                  # calling score function from Bowling Class with game object and printing score
 
-    #Function for testing all ones
+    #Function for testing all ones in the game
     def testAllOnes(self):
         self.rollMany(1, 20)
         assert self.game.score() == 20
